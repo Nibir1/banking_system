883c1824875f5494dd4658608eebc7bca05457cf
Nahasat Nibir <laptopbazaar@Nahasat-Nibir.local>
2024-10-13 01:03:39 +0600

    added viper for loading environment variables

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,18 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/banking_system?sslmode=disable"
+	"github.com/nibir1/banking_system/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
