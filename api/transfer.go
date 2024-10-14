f4331edc52851f8248032e7154f97706d991c354
Nahasat Nibir <laptopbazaar@Nahasat-Nibir.local>
2024-10-15 00:46:11 +0600

    transfer.go updated and transfer_test.go implemented

--- api/transfer.go
+++ api/transfer.go
@@ -11,7 +11,6 @@ import (
 	"github.com/nibir1/banking_system/token"
 )
 
-// createAccount functionalities for api
 type transferRequest struct {
 	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
 	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
@@ -30,6 +29,7 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 	if !valid {
 		return
 	}
+
 	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	if fromAccount.Owner != authPayload.Username {
 		err := errors.New("from account doesn't belong to the authenticated user")
@@ -37,7 +37,7 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		return
 	}
 
-	_, valid = server.validAccount(ctx, req.FromAccountID, req.Currency)
+	_, valid = server.validAccount(ctx, req.ToAccountID, req.Currency)
 	if !valid {
 		return
 	}
@@ -60,16 +60,17 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) (db.Account, bool) {
 	account, err := server.store.GetAccount(ctx, accountID)
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err, sql.ErrNoRows) {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
 			return account, false
 		}
+
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return account, false
 	}
 
 	if account.Currency != currency {
-		err := fmt.Errorf("account [%d] currecny mismatch: %s vs %s", accountID, account.Currency, currency)
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return account, false
 	}
